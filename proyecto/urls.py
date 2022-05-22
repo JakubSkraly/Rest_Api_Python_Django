33150375b9d506593ec90bf65a697f86628a77b8
Jakub Skraly <leonelinga18@outlook.es>
2022-05-21 21:06:10 -0500

    Modificación el urls.py

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# Descomentar cuando se ejecuta el collectstatic
+# from django.conf.urls.static import static
+# from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inicio.urls')),
     path('api/', include('api.urls')),
-]
+]# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Descomentar cuando se ejecuta el collectstatic
